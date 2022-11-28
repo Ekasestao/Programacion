6deeeb6234c1b7e6d16e4579bcf2f07bfde267ad
Ekaitz Campo <ekaitzcampo2002@gmail.com>
2022-11-28 04:53:25 +0100

    Actualizando c++

--- C++/Capítulo 2 Expresiones/Ejercicios/Ejercicio 7/Ejercicio7.cpp
+++ C++/Capítulo 2 Expresiones/Ejercicios/Ejercicio 7/Ejercicio7.cpp
@@ -1,18 +1,22 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
-int main(){
-    float a,b,c,media=0;
+int main()
+{
+  float a, b, c, media = 0;
 
-    cout<<"Ingrese su nota practica: "; cin>>a;
-    cout<<"Ingrese su nota teorica: "; cin>>b;
-    cout<<"Ingrese su nota participatoria: "; cin>>c;
+  cout << "Ingrese su nota practica: ";
+  cin >> a;
+  cout << "Ingrese su nota teorica: ";
+  cin >> b;
+  cout << "Ingrese su nota participatoria: ";
+  cin >> c;
 
-    media=(a*0.3)+(b*0.6)+(c*0.1);
+  media = (a * 0.3) + (b * 0.6) + (c * 0.1);
 
-    cout.precision(2);
-    cout<<"\nSu nota final es de: "<<media<<endl;
+  cout.precision(2);
+  cout << "\nSu nota final es de: " << media << endl;
 
-    return 0;
+  return 0;
 }
--- C++/Capítulo 2 Expresiones/Ejercicios/Ejercicio 8/Ejercicio8.cpp
+++ C++/Capítulo 2 Expresiones/Ejercicios/Ejercicio 8/Ejercicio8.cpp
@@ -1,18 +1,21 @@
-#include<iostream>
-#include<math.h> //Módulo math
+#include <iostream>
+#include <math.h> //Módulo math
 
 using namespace std;
 
-int main(){
-    float a,b,c;
+int main()
+{
+  float a, b, c;
 
-    cout<<"Digite el primer cateto: "; cin>>a;
-    cout<<"Digite el segundo cateto: "; cin>>b;
+  cout << "Digite el primer cateto: ";
+  cin >> a;
+  cout << "Digite el segundo cateto: ";
+  cin >> b;
 
-    c=sqrt(pow(a,2)+pow(b,2)); //Pow para elevar a n una variable o valor
+  c = sqrt(pow(a, 2) + pow(b, 2)); // Pow para elevar a n una variable o valor
 
-    cout.precision(2);
-    cout<<"El valor de la hipotenusa es: "<<c<<endl;
+  cout.precision(2);
+  cout << "El valor de la hipotenusa es: " << c << endl;
 
-    return 0;
+  return 0;
 }
