c0489bf65f287c0daa663b6d29222c28466fe62d
Ekaitz Campo <ekaitzcampo2002@gmail.com>
2022-12-02 06:10:03 +0100

    Cerrando proyecto 4

--- React/Lista_de_Tareas/src/App.js
+++ React/Lista_de_Tareas/src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import ListaDeTareas from "./componentes/ListaDeTareas";
+import freeCodeCampLogo from "./images/freecodecamp-logo.png";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="freecodecamp-logo-contenedor">
+        <img
+          src={freeCodeCampLogo}
+          className="freecodecamp-logo"
+          alt="Logo freeCodeCamp"
+        />
+      </div>
+      <div className="tareas-lista-principal">
+        <h1>Mis Tareas</h1>
+        <ListaDeTareas />
+      </div>
+    </div>
+  );
 }
 
 export default App;
