f1eeb2152abe1f1aa9d4f6d48d9b14d6fa321038
Ekaitz Campo <ekaitzcampo2002@gmail.com>
2022-12-20 01:36:04 +0100

    Cerrando TypeScript

--- TypeScript/dist/index.js
+++ TypeScript/dist/index.js
@@ -3,4 +3,39 @@ let mensaje = "Hola Mundo";
 mensaje = "Chanchito feliz";
 mensaje = "Chao Mundo";
 console.log(mensaje);
+console.log(typeof []);
+let extincionDinosaurios = 76000000;
+let dinosaurioFavorito = "T-rex";
+let extintos = true;
+function canchitoFeliz(config) {
+    return config;
+}
+let animales = ["chanchito", "feliz", "felipe"];
+let nums = [1, 2, 3];
+let checks = [];
+let num2 = [];
+let tupla = [1, ["chanchito feliz", "chanchito felipe"]];
+const chica = "s", mediana = "m", grande = "l", extragrande = "xl";
+var Talla;
+(function (Talla) {
+    Talla["Chica"] = "s";
+    Talla["Mediana"] = "m";
+    Talla["Grande"] = "l";
+    Talla["ExtraGrande"] = "xl";
+})(Talla || (Talla = {}));
+const variable1 = Talla.Grande;
+console.log(variable1);
+const estado = 2;
+const objeto = {
+    id: 1,
+    talla: Talla.Mediana,
+};
+objeto.nombre = "Hola Mundo";
+objeto.direccion = {
+    numero: 1,
+    calle: "Sotera de la Mier",
+    pais: "España",
+};
+const arr = [];
+console.log(objeto);
 //# sourceMappingURL=index.js.map
--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -5,3 +5,94 @@ mensaje = "Chanchito feliz";
 mensaje = "Chao Mundo";
 
 console.log(mensaje);
+console.log(typeof []);
+
+/**
+ * Tipos JS
+ * number
+ * string
+ * boolean
+ * null
+ * undefined
+ * object
+ * function
+ *
+ * Tipos TS
+ * any (No usar)
+ * unknown
+ * never
+ * arrays
+ * tuplas
+ * Enums
+ *
+ * Tipos inferidos
+ */
+
+let extincionDinosaurios = 76_000_000;
+let dinosaurioFavorito = "T-rex";
+let extintos = true;
+
+function canchitoFeliz(config: any) {
+  return config;
+}
+
+let animales: string[] = ["chanchito", "feliz", "felipe"];
+let nums: number[] = [1, 2, 3];
+let checks: boolean[] = [];
+let num2: Array<number> = [];
+
+// animales.map(x => x.) Sugiere tipo de dato
+
+let tupla: [number, string[]] = [1, ["chanchito feliz", "chanchito felipe"]];
+
+const chica = "s",
+  mediana = "m",
+  grande = "l",
+  extragrande = "xl";
+
+enum Talla {
+  Chica = "s", //n
+  Mediana = "m", //n+1
+  Grande = "l", //n+2
+  ExtraGrande = "xl", //n+3
+}
+
+const variable1 = Talla.Grande;
+console.log(variable1);
+
+const enum LoadingState { //Const antes de Enum para que el código JS quede más optimizado
+  Idle,
+  Loading,
+  Success,
+  Error,
+}
+
+const estado = LoadingState.Success; //Const variable para saber el valor de cada Enum
+
+// Type para crear un tipo personalizado
+type Direccion = {
+  numero: number;
+  calle: string;
+  pais: string;
+};
+
+type Persona = {
+  readonly id: number; //Readonly para no poder cambiar el valor
+  nombre?: string; //Poner ? para hacerla opcional
+  talla: Talla;
+  direccion?: Direccion;
+};
+
+const objeto: Persona = {
+  id: 1,
+  talla: Talla.Mediana,
+};
+objeto.nombre = "Hola Mundo"; //Añadir al objeto
+objeto.direccion = {
+  numero: 1,
+  calle: "Sotera de la Mier",
+  pais: "España",
+};
+
+const arr: Persona[] = [];
+console.log(objeto);
